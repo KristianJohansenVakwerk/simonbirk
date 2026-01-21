00e23367aa6c88635ccf884c961865149cc4d206
Kristian Johansen <contact@kristianjohansen.dk>
2026-01-21 11:06:26 +0100

    move menu down a bit

--- components/SettingsItemsMobile/SettingsItemsMobile.tsx
+++ components/SettingsItemsMobile/SettingsItemsMobile.tsx
@@ -5,56 +5,48 @@ import Text from '../shared/ui/Text/Text';
 import { QuerySettingsResult } from '@/sanity/types/sanity.types';
 import clsx from 'clsx';
 
-
 type Props = {
   settings: QuerySettingsResult;
 };
 const SettingsItemsMobile = (props: Props) => {
   const { settings } = props;
 
-  
-
   if (!settings) return;
 
- 
-
   return (
-    
-      <div className="fixed bottom-[16px] left-1 z-10 flex flex-col w-fit h-fit  items-start  gap-[6px]  lg:hidden">
-         <Link
-          href={'/'}
-          className={
-            'sticky top-[16px] lg:top-75 col-span-7 lg:col-span-4 flex gap-1  h-fit flex-row lg:flex-row lg:gap-1 lg:pl-1'
-          }
-          
-        >
-          <Text>Simon Birk</Text>
-          <Text>Photographer</Text>
-        </Link>
-        {settings.info?.map((item) => {
-          return (
-            <Box
-              key={item._key}
-              className={clsx('flex flex-row gap-1')}
-            >
-              <Text>{item.title}</Text>
+    <div className="fixed bottom-[8px] left-1 z-10 flex h-fit w-fit flex-col items-start gap-[6px] lg:hidden">
+      <Link
+        href={'/'}
+        className={
+          'sticky top-[16px] col-span-7 flex h-fit flex-row gap-1 lg:top-75 lg:col-span-4 lg:flex-row lg:gap-1 lg:pl-1'
+        }
+      >
+        <Text>Simon Birk</Text>
+        <Text>Photographer</Text>
+      </Link>
+      {settings.info?.map((item) => {
+        return (
+          <Box
+            key={item._key}
+            className={clsx('flex flex-row gap-1')}
+          >
+            <Text>{item.title}</Text>
 
-              {item.href ? (
-                <Link
-                  className="gap-0 font-sans text-base leading-none capsize"
-                  href={item.href}
-                  target="_blank"
-                >
-                  {item.hrefLabel}
-                </Link>
-              ) : (
-                <Text>{item.hrefLabel}</Text>
-              )}
-            </Box>
-          );
-        })}
-      </div>
-    
+            {item.href ? (
+              <Link
+                className="gap-0 font-sans text-base leading-none capsize"
+                href={item.href}
+                target="_blank"
+              >
+                {item.hrefLabel}
+              </Link>
+            ) : (
+              <Text>{item.hrefLabel}</Text>
+            )}
+          </Box>
+        );
+      })}
+    </div>
   );
 };
 
